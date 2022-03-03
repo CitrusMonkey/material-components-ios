e1df7a87d513a9bfe4f4618a5a222e562e1e8f88
CitrusMonkey <48776020+CitrusMonkey@users.noreply.github.com>
2022-03-02 23:58:49 -0600

    change order of sources parameter in Package file

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .target(
             name: "MaterialComponents",
-            sources: ["components"],
             dependencies: []),
+            sources: ["components"]
 //        .testTarget(
 //            name: "MaterialComponentsTests",
 //            dependencies: ["MaterialComponents"]),
